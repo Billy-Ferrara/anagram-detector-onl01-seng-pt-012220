ca27709725a4dc83e1ef654258ce711774cbc11a
Billy Ferrara <willfira717@gmail.com>
2020-04-01 17:53:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,18 @@
-# Your code goes here!
 class Anagram
 
-    attr_accessor :word
+  attr_accessor :word
 
   def initialize(word)
     @word = word
   end
 
-  def match(words)
-    words.select do |word|
+  def match(word_array)
+    # .select returns everything itterated into its own array hence "word_array"
+    # all of the words are itterating through |word| until a match is/isn't found
+    # word.split breaks everything down between the quotation marks then compares it against the words in the @words array 
+    word_array.select do |word|
       word.split("").sort == @word.split("").sort
-    end 
+    end
   end
 
 end
